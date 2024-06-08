969de2312287f3ff234658fa52062fef93685c58
alvi <ahalal82@gmail.com>
2024-06-08 16:58:25 +0300

    proccesing

--- GetSchema.go
+++ GetSchema.go
@@ -1,66 +1,37 @@
-package main
+package post05
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"strings"
+
 	_ "github.com/lib/pq"
-	"os"
 )
 
-func main() {
-	arguments := os.Args
-	if len(arguments) != 6 {
-		fmt.Println("Please provide: hostname port username password db")
-		return
-	}
+type Userdata struct {
+	ID          int
+	Username    string
+	Name        string
+	Surname     string
+	Description string
+}
 
-	host := arguments[1]
-	pgport := arguments[2]
-	username := arguments[3]
-	password := arguments[4]
-	dbname := arguments[5]
+var (
+	Hostname = ""
+	Port     = 2345
+	Username = ""
+	Password = ""
+	Database = ""
+)
 
-	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, pgport, username, password, dbname)
+func openConnection() (*sql.DB, error) {
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		Hostname, Port, Username, Password, Database)
 
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
-		fmt.Println("Open():", err)
-		return
+		return nil, err
 	}
-
-	defer db.Close()
-
-	rows, err := db.Query(`SELECT "datname" FROM "pg_database" WHERE datistemplate = false`)
-	if err != nil {
-		fmt.Println("Query():", err)
-		return
-	}
-
-	for rows.Next() {
-		var name string
-		err = rows.Scan(&name)
-		if err != nil {
-			fmt.Println("Scan():", err)
-			return
-		}
-		fmt.Println("*", name)
-	}
-	defer rows.Close()
-
-	query := `SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name`
-	rows, err = db.Query(query)
-	if err != nil {
-		fmt.Println("Query():", err)
-		return
-	}
-	for rows.Next() {
-		var name string
-		err = rows.Scan(&name)
-		if err != nil {
-			fmt.Println("Scan():", err)
-			return
-		}
-		fmt.Println("+T", name)
-	}
-
+	return db, nil
 }
